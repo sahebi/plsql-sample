472dacdcfc547d158495902f4e04f0789df623cd
Mahdi Sahebi <unlike_666@yahoo.com>
2017-02-09 16:44:36 +0330

    Learn MD and Change ms_week static date

--- System Functions/ms_week.sql
+++ System Functions/ms_week.sql
@@ -6,8 +6,8 @@ Select xxx.DT,
                to_char(dtm, 'd'),
                Decode(to_char(Lag(dtm) over (order by dt), 'd'), 6, 1, 0) change_week_flag
           From (
-                Select Hijri(Miladi('1395/01/01')+Level-1) dt,
-                       Miladi('1395/01/01')+Level-1 dtm
+                Select Hijri(Miladi(H_YEAR||'/01/01')+Level-1) dt,
+                       Miladi(H_YEAR||'/01/01')+Level-1 dtm
                   From Dual
                     Connect By Level <= 365
                ) xxx
